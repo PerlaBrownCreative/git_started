1897669404041da253d9aba425b516e70aaa866c
Perla Brown <62082149+PerlaBrownCreative@users.noreply.github.com>
2021-01-21 20:19:40 -0500

    Setup file structure

---
+++ app.js
@@ -0,0 +1,8 @@
+//git
+// git is a version control system used to track changes in your files. Used most notably by developers.
+//Git commits are checksumed using SHA-1, meaning no corrupt files go unnoticed without user's knowledge.
+//git has three states
+// - modified
+// file changed, but it has not yet been committed (ex: normal work, or saving a file)
+// - staging
+// 
