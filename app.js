4683c22cd53feef6c884c36a217a43533673dd89
Perla Brown <62082149+PerlaBrownCreative@users.noreply.github.com>
2021-01-21 21:21:51 -0500

    git it done

--- app.js
+++ app.js
@@ -12,4 +12,37 @@
 // to start version/track control
 //git init
 //creates new project
-//
+// your project should never be inside another project folder in order to avoid inverse tree tracking issues
+
+//stage our files for a commit
+//git add<file>
+
+//use git status to check on file staging
+
+//to commit your files, use a git commit - m "your message here"
+
+//to see all your commit history, use git log
+
+// to create new branch, use git branch <new_branch_name>
+//OR
+//to create new branch and switch to it, use git checkout -b <branch_name>
+
+//to switch between branches, use git checkout <branch_name>
+
+//to delete a branch git branch -D <branch_name>
+
+//git reset - to undelete changes
+
+//git revert HEAD 
+
+//to remove and unstagged commit, use git reset <file_name>
+
+//to remove an entire commit, use git reset --hard <commit hash BEFORE the ones you want removed>
+
+// to add remote branch, first check if any exist using git remote -v
+//if none exist, and you've created a repo on Github
+//run git remote add <name_of_remote_stream> <github_repo_url>
+
+//to push to Github
+//git push <name_of_remote_stream> <name of branch>
+//you must sit on the branch that you are pushing up
