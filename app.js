2d9f140ae28ba5b50e773f1ea91453af823eebaf
Perla Brown <62082149+PerlaBrownCreative@users.noreply.github.com>
2021-01-21 20:32:56 -0500

    added notes

--- app.js
+++ app.js
@@ -5,4 +5,11 @@
 // - modified
 // file changed, but it has not yet been committed (ex: normal work, or saving a file)
 // - staging
-// 
+// files that are marked to go into the next commit snapshot (utilizing git add <file_name> or git add . to add ever file in the folder)
+// - commit state
+// data safely stored in the local database(such as vm box, docker, computer etc)
+
+// to start version/track control
+//git init
+//creates new project
+//
